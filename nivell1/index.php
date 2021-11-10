6456e363acb243c4a4bb12d5a94af870a92f097e
ftrr <ftrrbnnn@gmail.com>
2021-11-10 09:46:35 +0100

    Tema 3 - Funcions - Sprint 1 - Exercici 2

--- nivell1/index.php
+++ nivell1/index.php
@@ -1,6 +1,9 @@
 <?php
 
-//Exercici 1
+//Exercici 2
+echo "<br><b>Exercici 1</b><br>";
+
+
 function parellImparell($valor){
 
 
@@ -10,6 +13,19 @@ function parellImparell($valor){
         echo $valor . ' es parell.';
     }
 }
-
-
 parellImparell(35);
+
+
+//Exercici 2
+echo "<br><br><b>Exercici 2</b><br>";
+
+function de2en2(){
+    $x = 0;
+    while($x<10){
+        $x++;
+        $x++;
+        echo $x . ", ";
+    }
+}
+de2en2();
+
